ae34a4b0bd73695e4965b7fc3ca835d1ee660f83
L-u-k-e-GIT <103580092+L-u-k-e-GIT@users.noreply.github.com>
2022-05-30 11:00:42 +0200

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,21 @@
+
+
+ 
+variable "MD_VIRTUAL_MACHINE_NAME" { 
+ description = "Virtual Machine name"
+ type = string
+ default = ""
+}
+
+variable "MD_RG_NAME" { 
+ description = "Virtual Machine name"
+ type = string
+ default = ""
+}
+
+variable "MD_VM_APP_ID"  { 
+ description = "ID of Vm Application"
+ type = string
+ default = ""
+}
+
---
+++ vm_applications.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+  
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>0.1"
+    }
+  }
+}
+
+
+resource "azapi_update_resource" "vm_applications_sccm" {
+  type      = "Microsoft.Compute/virtualMachines@2021-11-01"   //API version to use
+  name      = var.MD_VIRTUAL_MACHINE_NAME // virtual machine name 
+  parent_id = var.MD_RG_NAME
+
+  body = jsonencode({
+    
+  "properties": {
+    "applicationProfile": {
+      "galleryApplications": [
+        {
+          "order": 1,
+          "packageReferenceId": "${var.MD_VM_APP_ID} " ,
+         
+        }
+      ]
+    }
+  }
+  }
+  )
+}
+      
