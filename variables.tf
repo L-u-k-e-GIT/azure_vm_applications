41d0f67427bf9039e21f7be2f3df6a59783d5f1d
Luca Ferrandino <luca.ferrandino@windtre.it>
2022-06-17 17:08:21 +0200

    spaces

--- variables.tf
+++ variables.tf
@@ -1,26 +1,26 @@
-variable "MD_RUN"  { 
- description = "ID of Vm Application"
- type = number
- default = "0"
+variable "MD_RUN" {
+  description = "ID of Vm Application"
+  type        = number
+  default     = "0"
 }
 
 
- 
-variable "MD_VIRTUAL_MACHINE_NAME" { 
- description = "Virtual Machine name"
- type = string
- default = ""
+
+variable "MD_VIRTUAL_MACHINE_NAME" {
+  description = "Virtual Machine name"
+  type        = string
+  default     = ""
 }
 
-variable "MD_RG_NAME" { 
- description = "Virtual Machine name"
- type = string
- default = ""
+variable "MD_RG_NAME" {
+  description = "Virtual Machine name"
+  type        = string
+  default     = ""
 }
 
-variable "MD_VM_APP_ID"  { 
- description = "ID of Vm Application"
- type = string
- default = ""
+variable "MD_VM_APP_ID" {
+  description = "ID of Vm Application"
+  type        = string
+  default     = ""
 }
 
--- vm_applications.tf
+++ vm_applications.tf
@@ -1,33 +1,33 @@
 terraform {
   required_providers {
-  
+
     azapi = {
       source  = "azure/azapi"
       version = "~>0.1"
     }
   }
-} 
+}
 
 
 resource "azapi_update_resource" "vm_applications_sccm" {
   count     = var.MD_RUN
-  type      = "Microsoft.Compute/virtualMachines@2021-11-01"   //API version to use
-  name      = var.MD_VIRTUAL_MACHINE_NAME // virtual machine name 
-  parent_id = var.MD_RG_NAME // Resourse Group Name
+  type      = "Microsoft.Compute/virtualMachines@2021-11-01" //API version to use
+  name      = var.MD_VIRTUAL_MACHINE_NAME                    // virtual machine name 
+  parent_id = var.MD_RG_NAME                                 // Resourse Group Name
 
   body = jsonencode({
-    
-  "properties": {
-    "applicationProfile": {
-      "galleryApplications": [
-        {
-          "order": 1,
-          "packageReferenceId": "${var.MD_VM_APP_ID}" ,  // ID of VM Applications
-         
-        }
-      ]
+
+    "properties" : {
+      "applicationProfile" : {
+        "galleryApplications" : [
+          {
+            "order" : 1,
+            "packageReferenceId" : "${var.MD_VM_APP_ID}", // ID of VM Applications
+
+          }
+        ]
+      }
     }
-  }
-  }
+    }
   )
 }
